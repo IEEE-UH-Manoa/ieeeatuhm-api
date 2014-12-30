ce2d129b9fb1b48432a404b23c83b5671bcb3553
Kenny Luong <luong97@hawaii.edu>
2014-12-30 13:58:44 -1000

    Cleaned up routes and some code

--- server.js
+++ server.js
@@ -10,37 +10,45 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 
-var testURL = 'https://docs.google.com/spreadsheets/d/1RRZ_U3mcNJB3NpVmlW1F0KbIsJWkxmxMHgC8qYDchcs/pubhtml';
+// =======================
+// Some Data
+// =======================
+var eventSheetURL = 'https://docs.google.com/spreadsheets/d/1RRZ_U3mcNJB3NpVmlW1F0KbIsJWkxmxMHgC8qYDchcs/pubhtml';
+var aboutSheetURL = 'https://docs.google.com/spreadsheets/d/13VHmI6cCaqyxMpohc9gKj7-08D8Gdw7AWyrFfmnvdNE/pubhtml?gid=0&single=true';
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
 
-server.get('/', function (req, res, next) {
+// =======================
+// Routes
+// =======================
+server.get('/events', fetchEvents);
+server.get('/about', fetchAbout);
+server.get('/', sayHello);
+
+
+// =======================
+// Descriptions
+// =======================
+function sayHello(req, res, next){
   res.send("Hello, World!");
   return next();
-});
+}
 
-server.get('/events', function (req, res, next) {
-
+function fetchEvents(req, res, next){
   var options = {
-    key: testURL,
+    key: eventSheetURL,
     debug: true,
     callback: function(data, tabletop){
       res.send(data['2014-2015'].elements);
-      }
+    }
   };
 
   Tabletop.init(options);
   return next();
+}
 
-});
-
-server.get('/about', function (req, res, next) {
-
+function fetchAbout(req,res,next){
   var options = {
-    key: 'https://docs.google.com/spreadsheets/d/13VHmI6cCaqyxMpohc9gKj7-08D8Gdw7AWyrFfmnvdNE/pubhtml?gid=0&single=true',
+    key: aboutSheetURL,
     debug: true,
     simpleSheet: true,
     callback: function(data, tabletop){
@@ -50,8 +58,8 @@ server.get('/about', function (req, res, next) {
 
   Tabletop.init(options);
   return next();
+}
 
-});
 
 server.listen(16906, function () {
   console.log('%s listening at %s', server.name, server.url);
