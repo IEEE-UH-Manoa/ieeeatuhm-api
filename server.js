b63c936c4e169f802b9830b4e1f572af8cbb38a4
Kenny Luong <luong97@hawaii.edu>
2014-12-30 11:05:42 -1000

    Add initial restify server with event API support

---
+++ server.js
@@ -0,0 +1,42 @@
+var restify = require('restify');
+var Tabletop = require('tabletop');
+
+var server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+
+var testURL = 'https://docs.google.com/spreadsheets/d/1RRZ_U3mcNJB3NpVmlW1F0KbIsJWkxmxMHgC8qYDchcs/pubhtml';
+
+server.get('/echo/:name', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+
+server.get('/', function (req, res, next) {
+  res.send("Hello, World!");
+  return next();
+});
+
+server.get('/events', function (req, res, next) {
+
+  var options = {
+    key: testURL,
+    debug: true,
+    callback: function(data, tabletop, req, rest, next){
+      res.send(data['2014-2015'].elements);
+      }
+  };
+
+  Tabletop.init(options);
+  return next();
+
+});
+
+server.listen(16906, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
