c12a483a0bb543316d07d633da014d85f1a9466e
Kenny Luong <luong97@hawaii.edu>
2015-03-05 21:26:19 -1000

    Comments on run script

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,9 @@
-// A small example run script; remember to populate these environment variables!
+# run_script.sh
+# A small example run script; remember to populate these environment variables!
+# 
+# You *should* copy run_script.sh to run_script (it's set to ignore in the
+# .gitignore file), and then do a `source run_script` to run it.
+#
 IEEE_MONGO_URL="mongodb://ieee:password@something.mongolab.com:49171/database" \
 IEEE_TRELLO_KEY=WHOOO_TRELLO_KEY \
 node server.js
