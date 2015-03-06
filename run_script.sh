fbe4e673baf1ef8414b680b26106124d42db948a
Kenny Luong <luong97@hawaii.edu>
2015-03-05 21:25:13 -1000

    Added a run script for local debug

---
+++ run_script.sh
@@ -0,0 +1,4 @@
+// A small example run script; remember to populate these environment variables!
+IEEE_MONGO_URL="mongodb://ieee:password@something.mongolab.com:49171/database" \
+IEEE_TRELLO_KEY=WHOOO_TRELLO_KEY \
+node server.js
